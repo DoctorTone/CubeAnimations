f5863351ca76134f0af7b4272d7592a409c64f12
DoctorTone <drtonesoftware@gmail.com>
2020-01-31 18:18:36 +0000

    Inital commit.

---
+++ src/js/index.js
@@ -0,0 +1,266 @@
+import $ from "jquery";
+import * as THREE from "three";
+import { LineGeometry } from "three/examples/jsm/lines/LineGeometry";
+import { LineMaterial } from 'three/examples/jsm/lines/LineMaterial';
+import { Line2 } from 'three/examples/jsm/lines/Line2';
+
+import { BaseApp } from "./baseApp";
+import { APPCONFIG } from "./appConfig";
+import { LabelManager } from "./LabelManager";
+import controlkit from "controlkit";
+import bootstrap from "bootstrap";
+
+class Animations extends BaseApp {
+    constructor() {
+        super();
+        this.cameraRotate = false;
+        this.rotSpeed = Math.PI/20;
+        this.rotDirection = 1;
+        this.zoomingIn = false;
+        this.zoomingOut = false;
+        this.zoomSpeed = APPCONFIG.ZOOM_SPEED;
+
+        //Temp variables
+        this.tempVec = new THREE.Vector3();
+    }
+
+    setContainer(container) {
+        this.container = container;
+    }
+
+    init(container) {
+        this.container = container;
+        super.init(container);
+    }
+
+    addGroundPlane() {
+        const groundGeom = new THREE.PlaneBufferGeometry(APPCONFIG.GROUND_WIDTH, APPCONFIG.GROUND_HEIGHT, APPCONFIG.GROUND_SEGMENTS);
+        const groundMat = new THREE.MeshLambertMaterial( {color: APPCONFIG.GROUND_MATERIAL} );
+        const ground = new THREE.Mesh(groundGeom, groundMat);
+        ground.rotation.x = -Math.PI/2;
+        ground.position.y = 0;
+        this.root.add(ground);
+    }
+
+    createGUI() {
+        let guiWidth = $('#guiWidth').css("width");
+        guiWidth = parseInt(guiWidth, 10);
+        let gui = new controlkit();
+        gui.addPanel( {label: "Configuration", width: guiWidth, enable: false})
+            
+        this.gui = gui;
+    }
+
+    createScene() {
+        // Init base createsScene
+        super.createScene();
+        // Create root object.
+        this.root = new THREE.Object3D();
+        this.addToScene(this.root);
+        this.root.rotation.y = APPCONFIG.ROOT_ROTATE;
+
+        // Add ground
+        // this.addGroundPlane();
+
+        // Add blocks to scene
+        const blockMaterial = new THREE.MeshLambertMaterial({color: 0xcdcdcd});
+        const blockGeom = new THREE.BoxBufferGeometry(10, 10, 10);
+        const blockMesh = new THREE.Mesh(blockGeom, blockMaterial);
+        this.root.add(blockMesh);
+
+        this.createGUI();
+    }
+
+    update() {
+        let delta = this.clock.getDelta();
+
+        if (this.cameraRotate) {
+            this.root.rotation[this.rotAxis] += (this.rotSpeed * this.rotDirection * delta);
+        }
+
+        if(this.zoomingIn) {
+            this.tempVec.copy(this.camera.position);
+            this.tempVec.multiplyScalar(this.zoomSpeed * delta);
+            this.root.position.add(this.tempVec);
+            //DEBUG
+            //console.log("Root = ", this.root.position);
+        }
+
+        if(this.zoomingOut) {
+            this.tempVec.copy(this.camera.position);
+            this.tempVec.multiplyScalar(this.zoomSpeed * delta);
+            this.root.position.sub(this.tempVec);
+            //DEBUG
+            //console.log("Root = ", this.root.position);
+        }
+
+        super.update();
+    }
+
+    rotateCamera(status, direction) {
+        switch (direction) {
+            case APPCONFIG.RIGHT:
+                this.rotDirection = 1;
+                this.rotAxis = `y`;
+                break;
+
+            case APPCONFIG.LEFT:
+                this.rotDirection = -1;
+                this.rotAxis = `y`;
+                break;
+
+            case APPCONFIG.UP:
+                this.rotDirection = 1;
+                this.rotAxis = `x`;
+                break;
+
+            case APPCONFIG.DOWN:
+                this.rotDirection = -1;
+                this.rotAxis = `x`;
+                break;
+
+            default:
+                break;
+        };
+         
+        this.cameraRotate = status;
+    }
+
+    zoomIn(status) {
+        this.zoomingIn = status;
+    }
+
+    zoomOut(status) {
+        this.zoomingOut = status;
+    }
+}
+
+$(document).ready( () => {
+    
+    const container = document.getElementById("WebGL-Output");
+    const app = new Animations();
+    app.setContainer(container);
+
+    app.init(container);
+    app.createScene();
+
+    app.run();
+
+    // Elements
+    let rotateLeft = $("#rotateLeft");
+    let rotateRight = $("#rotateRight");
+    let rotateUp = $("#rotateUp");
+    let rotateDown = $("#rotateDown");
+    let zoomIn = $("#zoomIn");
+    let zoomOut = $("#zoomOut");
+
+    // Mouse interaction
+    rotateLeft.on("mousedown", () => {
+        app.rotateCamera(true, APPCONFIG.LEFT);
+    });
+
+    rotateLeft.on("mouseup", () => {
+        app.rotateCamera(false);
+    });
+
+    rotateRight.on("mousedown", () => {
+        app.rotateCamera(true, APPCONFIG.RIGHT);
+    });
+
+    rotateRight.on("mouseup", () => {
+        app.rotateCamera(false);
+    });
+
+    rotateUp.on("mousedown", () => {
+        app.rotateCamera(true, APPCONFIG.UP);
+    });
+
+    rotateUp.on("mouseup", () => {
+        app.rotateCamera(false);
+    });
+
+    rotateDown.on("mousedown", () => {
+        app.rotateCamera(true, APPCONFIG.DOWN);
+    });
+
+    rotateDown.on("mouseup", () => {
+        app.rotateCamera(false);
+    });
+
+    zoomIn.on("mousedown", () => {
+        app.zoomIn(true);
+    });
+
+    zoomIn.on("mouseup", () => {
+        app.zoomIn(false);
+    });
+
+    zoomOut.on("mousedown", () => {
+        app.zoomOut(true);
+    });
+
+    zoomOut.on("mouseup", () => {
+        app.zoomOut(false);
+    });
+
+    zoomOut.on("mousedown", () => {
+        app.zoomOut(true);
+    });
+
+    zoomOut.on("mouseup", () => {
+        app.zoomOut(false);
+    });
+
+    // Touch interaction
+    rotateLeft.on("touchstart", () => {
+        app.rotateCamera(true, APPCONFIG.LEFT);
+    });
+
+    rotateLeft.on("touchend", () => {
+        app.rotateCamera(false);
+    });
+
+    rotateRight.on("touchstart", () => {
+        app.rotateCamera(true, APPCONFIG.RIGHT);
+    });
+
+    rotateRight.on("touchend", () => {
+        app.rotateCamera(false);
+    });
+
+    rotateUp.on("touchstart", () => {
+        app.rotateCamera(true, APPCONFIG.UP);
+    });
+
+    rotateUp.on("touchend", () => {
+        app.rotateCamera(false);
+    });
+
+    rotateDown.on("touchstart", () => {
+        app.rotateCamera(true, APPCONFIG.DOWN);
+    });
+
+    rotateDown.on("touchend", () => {
+        app.rotateCamera(false);
+    });
+
+    zoomIn.on("touchstart", () => {
+        app.zoomIn(true);
+    });
+
+    zoomIn.on("touchend", () => {
+        app.zoomIn(false);
+    });
+
+    zoomOut.on("touchstart", () => {
+        app.zoomOut(true);
+    });
+
+    zoomOut.on("touchend", () => {
+        app.zoomOut(false);
+    });
+
+    $("#info").on("click", () => {
+        $("#infoModal").modal();
+    });
+});
